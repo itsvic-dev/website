f2bc4aac2fdb461a89863938633ded06d0541702
omame <me@omame.xyz>
2021-10-08 19:02:39 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+/*  omame's website
+ *  Copyright (C) 2021 omame <me@omame.xyz>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React from 'react'
 import tw from 'twin.macro'
 import omame from '@/assets/icon.webp'
 import zzz from '@/assets/1f4a4.svg'
 import CyberOS from '@/assets/cyberos.webp'
+import PlugifyWeb from '@/assets/plugify-web.webp'
 import '@fontsource/rubik/300.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiscord, faGitAlt, faGithub, faGitlab, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
@@ -19,45 +37,56 @@ const App = (): JSX.Element => {
 	return (
 		<div css={tw`w-full flex flex-col items-center`}>
 			<div css={tw`w-full flex flex-col items-center p-8 max-w-container`}>
-				<div css={tw`w-full flex justify-between flex-col lg:flex-row`}>
+				<div css={tw`w-full flex justify-between flex-col lg:flex-row pt-16`}>
 					<div css={tw`flex flex-col lg:flex-row items-center`}>
 						<img src={omame} css={tw`rounded-full w-64 h-auto shadow-lg`}/>
 						<div css={tw`lg:ml-16 text-center lg:text-left`}>
-							<p css={tw`text-6xl font-light pt-8 lg:pt-0`}>omame</p>
-							<p css={tw`text-2xl pt-4`}>just a sleepy kot <img src={zzz} css={tw`inline-block w-8 ml-2 h-auto`}/></p>
+							<h1 css={tw`text-6xl font-light pt-8 lg:pt-0`}>omame</h1>
+							<h3 css={tw`text-2xl pt-4`}>just a sleepy kot <img src={zzz} css={tw`inline-block w-8 ml-2 h-auto`}/></h3>
 						</div>
 					</div>
 
 					<div css={tw`flex flex-col lg:flex-row text-3xl justify-center items-center pt-4 lg:pt-0`}>
 						<div css={tw`flex lg:flex-col text-3xl justify-center items-center`}>
-							<SocialLink href="https://gitlab.com/omaemae">
+							<SocialLink rel="noreferrer" target="_blank" href="https://gitlab.com/omaemae">
 								<FontAwesomeIcon icon={faGitlab} />
 							</SocialLink>
-							<SocialLink href="https://dir.omame.xyz">
+							<SocialLink rel="noreferrer" target="_blank" href="https://dir.omame.xyz">
 								<FontAwesomeIcon icon={faFolder} />
 							</SocialLink>
 						</div>
 						<div css={tw`flex lg:flex-col text-3xl justify-center items-center lg:ml-4`}>
-							<SocialLink href="https://twitter.com/omametech">
+							<SocialLink rel="noreferrer" target="_blank" href="https://twitter.com/omametech">
 								<FontAwesomeIcon icon={faTwitter} />
 							</SocialLink>
-							<SocialLink href="https://discord.gg/WSMyh5fdUg">
+							<SocialLink rel="noreferrer" target="_blank" href="https://discord.gg/WSMyh5fdUg">
 								<FontAwesomeIcon icon={faDiscord} />
 							</SocialLink>
-							<SocialLink href="https://github.com/omaemae">
+							<SocialLink rel="noreferrer" target="_blank" href="https://github.com/omaemae">
 								<FontAwesomeIcon icon={faGithub} />
 							</SocialLink>
-							<SocialLink href="https://youtube.com/omame">
+							<SocialLink rel="noreferrer" target="_blank" href="https://youtube.com/omame">
 								<FontAwesomeIcon icon={faYoutube} />
 							</SocialLink>
-							<SocialLink href="mailto:me@omame.xyz">
+							<SocialLink rel="noreferrer" target="_blank" href="mailto:me@omame.xyz">
 								<FontAwesomeIcon icon={faEnvelope} />
 							</SocialLink>
 						</div>
 					</div>
 				</div>
 
-				<p css={tw`text-5xl pt-16 font-light`}>projects</p>
+				<h2 css={tw`text-5xl pt-16 font-light`}>projects</h2>
+
+				<Project
+					name="Plugify"
+					image={PlugifyWeb}
+					description="Plugify is a chat app designed with usability in mind. I'm the creator, designer and maintainer of the project."
+					links={{
+						'https://plugify.cf': faGlobe,
+						'https://twitter.com/PlugifyChat': faTwitter,
+						'https://gitlab.com/plugify-tf': faGitlab
+					}}
+				/>
 
 				<Project
 					name="CyberOS"
@@ -66,7 +95,7 @@ const App = (): JSX.Element => {
 					links={{
 						'https://getcyberos.org': faGlobe,
 						'https://twitter.com/CyberOS_Linux': faTwitter,
-						'https://git.omame.tech/CyberOS': faGitAlt
+						'https://git.omame.xyz/CyberOS': faGitAlt
 					}}
 				/>
 
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,20 @@
+/*  omame's website
+ *  Copyright (C) 2021 omame <me@omame.xyz>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
@@ -13,7 +30,7 @@ export type ProjectProps = {
 const Project = (props: ProjectProps): JSX.Element => {
 	return (
 		<div css={tw`shadow-xl m-8 p-8 bg-bgcolor-primary rounded-2xl flex flex-col lg:flex-row w-full justify-start`}>
-			<a href={props.image}>
+			<a rel="noreferrer" target="_blank" href={props.image}>
 				<img src={props.image} css={tw`w-full lg:w-96 rounded-xl shadow-md`}/>
 			</a>
 			<div css={tw`text-center lg:text-left pt-8 lg:pt-0 lg:pl-8 flex flex-col justify-between lg:w-2/3`}>
@@ -23,7 +40,7 @@ const Project = (props: ProjectProps): JSX.Element => {
 				</div>
 				<p css={tw`pt-8 text-3xl`}>
 					{Object.keys(props.links).map((link) => (
-						<a key={link} href={link} css={tw`m-2 hover:text-gray-400 transition-colors duration-200 ease-in-out`}>
+						<a key={link} rel="noreferrer" target="_blank" href={link} css={tw`m-2 hover:text-gray-400 transition-colors duration-200 ease-in-out`}>
 							<FontAwesomeIcon icon={props.links[link]} />
 						</a>
 					))}
